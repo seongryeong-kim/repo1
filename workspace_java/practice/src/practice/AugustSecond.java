84a86733f37e98f882590166c712a5e8450bfc98
김성령 <kimsr5383@gmail.com>
2024-08-05 17:32:46 +0900

    8/5 서블릿

---
+++ workspace_java/practice/src/practice/AugustSecond.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class AugustSecond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s="-1234";
+		int answer=0;
+		answer=Integer.parseInt(s);
+		System.out.println(answer);
+	}
+
+}
