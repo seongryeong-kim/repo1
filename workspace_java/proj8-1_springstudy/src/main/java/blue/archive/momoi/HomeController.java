556e90379c6d3cdc33c792b876ab71e406ab568f
김성령 <kimsr5383@gmail.com>
2024-09-28 16:13:39 +0900

    9/28 어제 복습한 내용 한번 더 정리

--- workspace_java/proj8-1_springstudy/src/main/java/blue/archive/momoi/HomeController.java
+++ workspace_java/proj8-1_springstudy/src/main/java/blue/archive/momoi/HomeController.java
@@ -24,22 +24,24 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		// logger 가 더 강한 print out이라고 생각하면 될 듯  
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
+		// serverTime 이라는 el태그칸에 formattedDate 가 보임
 		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("name", "momo" );
 		
 		return "home";
 	}
 	
+	// name 이라는 경로로 접속 했을 때 
 	@RequestMapping(value = "/name", method = RequestMethod.GET)
 	public String home0(Locale locale, Model model) {
 		
+		// home.jsp 파일 name 이라는 el태그에 momo라는 텍스트가 나오도록 
 		model.addAttribute("name", "momo" );
 		
 		return "home";
