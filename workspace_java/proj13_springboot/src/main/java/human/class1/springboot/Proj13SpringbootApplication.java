40d950b334ffd7f1b53fc3f80e7c87cc2c06bf76
김성령 <kimsr5383@gmail.com>
2024-10-30 17:08:00 +0900

    10/30 spring  boot

---
+++ workspace_java/proj13_springboot/src/main/java/human/class1/springboot/Proj13SpringbootApplication.java
@@ -0,0 +1,13 @@
+package human.class1.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj13SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj13SpringbootApplication.class, args);
+	}
+
+}
